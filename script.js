4495279c5b59cae68d29778e046843823deff134
Leonardo Araujo <33550045+araujoleo@users.noreply.github.com>
2022-09-12 17:46:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 function verificar(){
     const personagem = document.getElementsByName('personagem')
-    const tempo = document.getElementsByName('tempo')
+    const linhaTemporal = document.getElementsByName('tempo')
     const res = document.querySelector('div.res')
     const img = document.createElement('img')
 
     
 
-    if(personagem[0].checked && tempo[0].checked){
+    if(personagem[0].checked && linhaTemporal[0].checked){
+        res.innerHTML = ""
         img.setAttribute('src', 'img/jonas1.png')
-    } else if(personagem[0].checked && tempo[1].checked){
+    } else if(personagem[0].checked && linhaTemporal[1].checked){
+        res.innerHTML = ""
         img.setAttribute('src', 'img/jonas2.png')
-    } else if(personagem[1].checked && tempo[0].checked){
+    } else if(personagem[1].checked && linhaTemporal[0].checked){
+        res.innerHTML = ""
         img.setAttribute('src', 'img/elisabeth1.png')
-    } else if(personagem[1].checked && tempo[1].checked){
+    } else if(personagem[1].checked && linhaTemporal[1].checked){
+        res.innerHTML = ""
         img.setAttribute('src', 'img/elisabeth2.png')
-    } else if(personagem[2].checked && tempo[0].checked){
+    } else if(personagem[2].checked && linhaTemporal[0].checked){
+        res.innerHTML = ""
         img.setAttribute('src', 'img/magnus1.png')
-    } else if(personagem[2].checked && tempo[1].checked){
+    } else if(personagem[2].checked && linhaTemporal[1].checked){
+        res.innerHTML = ""
         img.setAttribute('src', 'img/magnus2.png')
-    } else if(personagem[3].checked && tempo[0].checked){
+    } else if(personagem[3].checked && linhaTemporal[0].checked){
+        res.innerHTML = ""
         img.setAttribute('src', 'img/franziska1.png')
-    } else if(personagem[3].checked && tempo[1].checked){
+    } else if(personagem[3].checked && linhaTemporal[1].checked){
+        res.innerHTML = ""
         img.setAttribute('src', 'img/franziska2.png')
     } else{
         res.innerHTML = '[ERRO] Selecione as opções corretamente!'
